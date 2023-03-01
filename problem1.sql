4b48ec2c4ac5537f6c19bcfba1596b06653d7c7e
Rey V <radiativedreams@gmail.com>
2023-03-01 04:54:44 +0000

    completed view

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+create view firstname_lastname as select firstname, lastname from site_user; 
+-- a view is sql is a virtual table
+-- using a view means its a subsection of the data we can query to
